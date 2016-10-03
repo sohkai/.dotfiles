97240df89fe3d7b4f85d374d93eaa9bb020931b5
Brett Sun <qisheng.brett.sun@gmail.com>
2016-10-03 13:58:19 +0200

    refactor(bash): split configuration into multiple logical units

---
+++ bash.d/bindings.sh
@@ -0,0 +1,6 @@
+################
+# Key bindings #
+################
+
+bind "\C-j menu-complete" # Forward cycle through completions
+bind "\C-k menu-complete-backward" # Backward cycle through completions
---
+++ bash.d/plugins.sh
@@ -0,0 +1,7 @@
+####################
+# External plugins #
+####################
+
+source_file ~/.iterm2_shell_integration.bash # iTerm shell integration
+source_file ~/.dotfiles/modules/grc/grc.bashrc # grc setup
+source_file ~/.dotfiles/bash.d/external/tab.bash # OSX new tab
