de6a0638856d1f67b9bfcc7de216ad12647c352b
Brett Sun <qisheng.brett.sun@gmail.com>
2016-10-03 14:15:03 +0200

    feature(OS X): add aliases
    Use a similar alias set to prezto

---
+++ osx.d/alias.sh
@@ -0,0 +1,12 @@
+#########################################################################
+#                               Aliases                                 #
+# (see https://github.com/sorin-ionescu/prezto/tree/master/modules/osx) #
+#########################################################################
+
+# No point doing anything else if we're not on OSX
+if [[ "$OSTYPE" != darwin* ]]; then
+  return 1
+fi
+
+alias pbc='pbcopy'
+alias pbp='pbpaste'
