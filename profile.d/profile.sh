4e31ed6122476414d47410d785b7b1b332ba8074
Brett Sun <qisheng.brett.sun@gmail.com>
2016-10-04 02:00:09 +0200

    fix(shell): use bash-safe fortune cookie

--- profile.d/profile.sh
+++ profile.d/profile.sh
@@ -1,11 +1,11 @@
 ##### FIRST THINGS FIRST #####
 
-# Gotta get my fortune
-if (( $+commands[fortune] )); then
-  if [[ -t 0 || -t 1 ]]; then
-    fortune -s
-    print
-  fi
+# Gotta get mai fortune cookiez
+if type fortune &> /dev/null ; then
+    if [[ -t 0 || -t 1 ]]; then
+        fortune -s
+        echo
+    fi
 fi
 
 
