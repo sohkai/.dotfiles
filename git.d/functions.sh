0d7e53a6a4cdc00eaff8c393033c7e03b0d12cb3
Brett Sun <qisheng.brett.sun@gmail.com>
2017-02-01 18:29:38 +0100

    fix(git): add missing functions from prezto

---
+++ git.d/functions.sh
@@ -0,0 +1,34 @@
+################################################################################
+#                                 Functions                                    #
+# Adapted from https://github.com/sorin-ionescu/prezto/tree/master/modules/git #
+################################################################################
+
+# Displays the current Git branch.
+git-branch-current() {
+    if ! git rev-parse 2> /dev/null; then
+        print "$0: not a repository: $PWD" >&2
+        return 1
+    fi
+
+    local ref="$(git symbolic-ref HEAD 2> /dev/null)"
+
+    if [[ -n "$ref" ]]; then
+        print "${ref#refs/heads/}"
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Displays the path to the working tree root.
+git-root() {
+    local root="$(git rev-parse --show-toplevel 2> /dev/null)"
+
+    if [[ -n "$root" ]]; then
+        print "$root"
+        return 0
+    else
+        print "$0: not a repository work tree: $PWD" >&2
+        return 1
+    fi
+}
