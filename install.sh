c5d043e0cd64552ec05a71b5ee41218854a7f803
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-04 15:53:35 +0200

    fix(install): add message to set up user's git config

--- install.sh
+++ install.sh
@@ -101,5 +101,6 @@ echo "All done! Phew, thanks for waiting :)."
 echo "There's a few manual steps left, that you'll need to finish by yourself:"
 echo
 echo "  - Some homebrewed apps may require manual setup, please see the logs above"
+echo "  - Please set up your git user config (see https://help.github.com/articles/setting-your-username-in-git/)"
 echo "  - Please set up SSH keys for Github by visiting https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/"
 echo
