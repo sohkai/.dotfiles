f4727298cb3c0b0fad565e3fc70e721add0a3f73
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-07 13:06:55 +0200

    feat(shell): add aliases for fzf git helpers^git

--- zsh.d/aliases.zsh
+++ zsh.d/aliases.zsh
@@ -6,19 +6,13 @@ alias resource='source $HOME/.zshrc && source $HOME/.zshenv'
 
 # Disable correction.
 alias ack='nocorrect ack'
-alias cd='nocorrect cd'
-alias cp='nocorrect cp'
 alias ebuild='nocorrect ebuild'
 alias gcc='nocorrect gcc'
 alias gist='nocorrect gist'
-alias grep='nocorrect grep'
 alias heroku='nocorrect heroku'
 alias ln='nocorrect ln'
 alias man='nocorrect man'
-alias mkdir='nocorrect mkdir'
-alias mv='nocorrect mv'
 alias mysql='nocorrect mysql'
-alias rm='nocorrect rm'
 
 # Disable globbing.
 alias bower='noglob bower'
