c476c4e5083a905e41283987c1db632a2afb3dd3
Brett Sun <qisheng.brett.sun@gmail.com>
2018-08-08 10:14:50 +0200

    fix(osx): remap global cursor shortcuts to avoid overriding vim navigation

--- zsh.d/bindings.zsh
+++ zsh.d/bindings.zsh
@@ -8,5 +8,10 @@ bindkey -M viins '^E' vi-add-eol
 bindkey -M vicmd 'q' push-line # Reset the current prompt but save it for the next command
 bindkey -M viins ' ' magic-space # Magic space completion for history
 
+bindkey -M viins '^j' history-substring-search-down
+bindkey -M viins '^k' history-substring-search-up
+bindkey -M viins '^h' backward-char
+bindkey -M viins '^l' forward-char
+
 # Ranger
 is_defined ranger && bindkey -s '^O' "ranger_cd\n"
